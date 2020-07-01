2a6b1dcfb1293e9112cbf98759a8af8d5d93fab5
BZIvanov <biser.z.ivanov@gmail.com>
2020-07-01 08:33:39 +0300

    auth module add

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RouterModule, Routes } from '@angular/router';
+
+import { RegisterComponent } from './components/register/register.component';
+
+const routes: Routes = [
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+];
+
+@NgModule({
+  declarations: [RegisterComponent],
+  imports: [CommonModule, RouterModule.forChild(routes)],
+})
+export class AuthModule {}
