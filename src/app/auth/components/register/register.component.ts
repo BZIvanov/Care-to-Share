c07f36ac42d1fb75c4e9fdacd6eb11bc544db5d6
BZIvanov <biser.z.ivanov@gmail.com>
2020-07-01 17:14:44 +0300

    install ngrx, add register action

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+
+import { registerAction } from '../../store/actions';
 
 @Component({
   selector: 'app-register',
@@ -9,7 +12,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class RegisterComponent implements OnInit {
   form: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private store: Store) {}
 
   ngOnInit(): void {
     this.initializeForm();
@@ -26,5 +29,6 @@ export class RegisterComponent implements OnInit {
 
   onSubmit(): void {
     console.log('submit', this.form.value, this.form.valid);
+    this.store.dispatch(registerAction(this.form.value));
   }
 }
