6f452924672f407ed0a331fcfdf7616c6616b157
BZIvanov <biser.z.ivanov@gmail.com>
2020-07-02 17:51:00 +0300

    login component

---
+++ src/app/auth/components/login/login.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import { loginAction } from '../../store/actions/login.action';
+import {
+  isSubmittingSelector,
+  validationErrorsSelector,
+} from 'src/app/auth/store/selectors';
+import { LoginRequestInterface } from 'src/app/auth/types/login-request.interface';
+import { BackendErrorsInterface } from 'src/app/shared/types/backend-errors.interface';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  form: FormGroup;
+  isSubmitting$: Observable<boolean>;
+  backendErrors$: Observable<BackendErrorsInterface | null>;
+
+  constructor(private fb: FormBuilder, private store: Store) {}
+
+  ngOnInit(): void {
+    this.initializeForm();
+    this.initializeValues();
+  }
+
+  initializeValues(): void {
+    this.isSubmitting$ = this.store.pipe(select(isSubmittingSelector));
+    this.backendErrors$ = this.store.pipe(select(validationErrorsSelector));
+  }
+
+  initializeForm(): void {
+    this.form = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    const request: LoginRequestInterface = {
+      user: this.form.value,
+    };
+    this.store.dispatch(loginAction({ request }));
+  }
+}
