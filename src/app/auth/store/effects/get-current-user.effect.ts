d97ea36fdc958123365be6ecf029374a4eb5d729
BZIvanov <biser.z.ivanov@gmail.com>
2020-07-02 19:10:42 +0300

    fetch user on app reload

---
+++ src/app/auth/store/effects/get-current-user.effect.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { createEffect, Actions, ofType } from '@ngrx/effects';
+import { map, catchError, switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
+
+import { AuthService } from '../../services/auth.service';
+import { CurrentUserInterface } from '../../../shared/types/current-user.interface';
+import { PersistanceService } from '../../../shared/services/persistance.service';
+import {
+  getCurrentUserAction,
+  getCurrentUserSuccessAction,
+  getCurrentUserFailureAction,
+} from '../actions/get-current-user.action';
+
+@Injectable()
+export class GetCurrentUserEffect {
+  getCurrentUser$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(getCurrentUserAction),
+      switchMap(() => {
+        const token = this.persistanceService.get('accessToken');
+
+        if (!token) {
+          return of(getCurrentUserFailureAction());
+        }
+
+        return this.authService.getCurrentUser().pipe(
+          map((currentUser: CurrentUserInterface) => {
+            return getCurrentUserSuccessAction({ currentUser });
+          }),
+
+          catchError(() => {
+            return of(getCurrentUserFailureAction());
+          })
+        );
+      })
+    )
+  );
+
+  constructor(
+    private actions$: Actions,
+    private authService: AuthService,
+    private persistanceService: PersistanceService
+  ) {}
+}
