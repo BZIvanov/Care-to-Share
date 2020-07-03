1040af07aced8f4194e7f27dab1bbe879d0d8577
BZIvanov <ivanov.biser@gmx.com>
2020-07-03 11:18:17 +0300

    banner, loading and error message component

---
+++ src/app/shared/modules/banner/banner.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BannerComponent } from './components/banner/banner.component';
+
+@NgModule({
+  declarations: [BannerComponent],
+  imports: [CommonModule],
+  exports: [BannerComponent],
+})
+export class BannerModule {}
--- src/app/shared/modules/feed/feed.module.ts
+++ src/app/shared/modules/feed/feed.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 
@@ -7,13 +8,18 @@ import { FeedComponent } from './components/feed/feed.component';
 import { FeedService } from 'src/app/shared/modules/feed/services/feed.service';
 import { GetFeedEffect } from 'src/app/shared/modules/feed/store/effects/get-feed.effect';
 import { reducers } from 'src/app/shared/modules/feed/store/reducers';
+import { ErrorMessageModule } from 'src/app/shared/modules/error-message/error-message.module';
+import { LoadingModule } from 'src/app/shared/modules/loading/loading.module';
 
 @NgModule({
   declarations: [FeedComponent],
   imports: [
     CommonModule,
+    RouterModule,
     EffectsModule.forFeature([GetFeedEffect]),
     StoreModule.forFeature('feed', reducers),
+    ErrorMessageModule,
+    LoadingModule,
   ],
   exports: [FeedComponent],
   providers: [FeedService],
