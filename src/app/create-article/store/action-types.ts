6a21314b239072f33da37031b6c22e1d8865e869
BZIvanov <biser.z.ivanov@gmail.com>
2020-07-04 17:58:53 +0300

    create article form and reducer

---
+++ src/app/create-article/store/action-types.ts
@@ -0,0 +1,5 @@
+export enum ActionTypes {
+  CREATE_ARTICLE = '[Create article] Create Article',
+  CREATE_ARTICLE_SUCCESS = '[Create article] Create Article success',
+  CREATE_ARTICLE_FAILURE = '[Create article] Create Article failure',
+}
---
+++ src/app/create-article/store/effects/create-article.effect.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { createEffect, Actions, ofType } from '@ngrx/effects';
+import { map, catchError, switchMap, tap } from 'rxjs/operators';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
+
+import { CreateArticleService } from '../../services/create-article.service';
+import {
+  createArticleSuccessAction,
+  createArticleFailureAction,
+  createArticleAction,
+} from 'src/app/create-article/store/actions/create-article.action';
+import { ArticleInterface } from 'src/app/shared/types/article.interface';
+
+@Injectable()
+export class CreateArticleEffect {
+  createArticle$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(createArticleAction),
+      switchMap(({ articleInput }) => {
+        return this.createArticleService.createArticle(articleInput).pipe(
+          map((article: ArticleInterface) => {
+            return createArticleSuccessAction({ article });
+          }),
+
+          catchError((errorResponse: HttpErrorResponse) => {
+            return of(
+              createArticleFailureAction({ errors: errorResponse.error.errors })
+            );
+          })
+        );
+      })
+    )
+  );
+
+  redirectAfterCreate$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(createArticleSuccessAction),
+        tap(({ article }) => {
+          this.router.navigate(['/articles', article.slug]);
+        })
+      ),
+    { dispatch: false }
+  );
+
+  constructor(
+    private actions$: Actions,
+    private createArticleService: CreateArticleService,
+    private router: Router
+  ) {}
+}
